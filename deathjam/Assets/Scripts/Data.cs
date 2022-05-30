ee671fe3c529ef6fa7d3742fcc6175a155e9ebda
itaisinger <itai5000si2@gmail.com>
2022-05-30 07:07:26 +0300

    dunno

--- deathjam/Assets/Scripts/Data.cs
+++ deathjam/Assets/Scripts/Data.cs
@@ -6,10 +6,4 @@ using UnityEngine.SceneManagement;
 public static class Data
 {
     public static int Deaths = 0;
-
-    // public Data()
-    // {
-    //     Deaths = 0;
-    //     DontDestroyOnLoad(this.gameObject);
-    // }
 }
